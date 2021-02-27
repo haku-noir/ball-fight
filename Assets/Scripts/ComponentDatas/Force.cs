c754a8d20ec6905739b271b9b22ecd5914656579
haku-noir <hakunoir.00@gmail.com>
2021-02-27 18:49:54 +0900

    add: MovePlayerSystem

---
+++ Assets/Scripts/ComponentDatas/Force.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct Force : IComponentData
+{
+    public float3 Value;
+}
---
+++ Assets/Scripts/Systems/Player/MovePlayerSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+using Unity.Jobs;
+
+public class MovePlayerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+
+        Entities
+            .WithAll<PlayerTag>()
+            .ForEach((ref PhysicsVelocity physicsVelocity, in PhysicsMass physicsMass, in Force force) =>
+            {
+                physicsVelocity.Linear += physicsMass.InverseMass * force.Value * deltaTime;
+            })
+            .Run();
+    }
+}
