fd913d9854184c3202792bab80629708cf5428fe
haku-noir <hakunoir.00@gmail.com>
2021-02-27 19:07:38 +0900

    add: ChangePlayerForceSystem

---
+++ Assets/Scripts/Systems/Player/ChangePlayerForceSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ChangePlayerForceSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Entities
+            .WithAll<PlayerTag>()
+            .ForEach((ref Force force) =>
+            {
+                force.Value = math.float3(horizontalInput * 10, 0, verticalInput * 10);
+            })
+            .Run();
+    }
+}
