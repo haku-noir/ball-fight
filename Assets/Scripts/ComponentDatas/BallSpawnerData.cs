a44f14317d214eab4db50dd27ffe38036e2ca5e2
haku-noir <hakunoir.00@gmail.com>
2021-03-18 06:39:19 +0900

    add: SpawnBallSystem

---
+++ Assets/Scripts/ComponentDatas/BallSpawnerData.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct BallSpawnerData : IComponentData
+{
+    public Entity BallPrefabEntitiy;
+    public float3 Position;
+}
---
+++ Assets/Scripts/Systems/Player/SpawnBallSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class SpawnBallSystem : SystemBase
+{
+    private EndInitializationEntityCommandBufferSystem entityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        entityCommandBufferSystem = World.GetOrCreateSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var commandBuffer = entityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+
+        Dependency = Entities
+            .WithAll<SpawnTag>()
+            .ForEach((Entity entity, int entityInQueryIndex, in BallSpawnerData ballSpawnerData, in LocalToWorld localToWorld) =>
+            {
+                var ballInstance = commandBuffer.Instantiate(entityInQueryIndex, ballSpawnerData.BallPrefabEntitiy);
+
+                commandBuffer.SetComponent(entityInQueryIndex, ballInstance, new Translation { Value = ballSpawnerData.Position });
+                commandBuffer.RemoveComponent<SpawnTag>(entityInQueryIndex, entity);
+            })
+            .ScheduleParallel(Dependency);
+
+        entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
